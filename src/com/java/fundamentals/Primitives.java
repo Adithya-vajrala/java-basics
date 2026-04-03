7bfd462e9992ccb4a5eea37a8af4575636e05ec2
Adithya-vajrala <vajrlaadithya@gmail.com>
2026-04-03 15:07:08 +0530

    feet: Primitives added

---
+++ src/com/java/fundamentals/Primitives.java
@@ -0,0 +1,44 @@
+package com.java.fundamentals;
+
+public class Primitives {
+    public static void main(String[] args) {
+        byte age;
+        byte subject1Marks, subject2Marks, subject3Marks, subject4Marks;
+        byte discountPercentage;
+        byte rating;
+        age = 100 ;
+        subject1Marks = 98;
+        subject2Marks = 67;
+        subject3Marks = 79;
+        subject4Marks = 89;
+        System.out.println("Age: "+age);
+        System.out.println("subject 1 Marks: " + subject1Marks + ", subject 2 Marks: " + subject2Marks + ", subject 3 Marks: " + subject3Marks + ", subject 4 Marks: " + subject4Marks);
+        System.out.println("-------------------------------------");
+        System.out.println("subject 1 Marks: " + subject1Marks);
+        System.out.println("subject 2 Marks: " + subject2Marks);
+        System.out.println("subject 3 Marks: " + subject3Marks);
+        System.out.println("subject 4 Marks: " + subject4Marks);
+        System.out.println("--------------------------------------");
+        short totalMarks = 645;
+        short manufacturingYear = 2026;
+        short expiryDays = 180;
+        System.out.println("Manufacturing Year: " + manufacturingYear + ", Days in Expiry: " + expiryDays);
+        /*
+        salary,rollNumber,otp,cityPopulation,pincode,
+        */
+        int salary = 100000;
+        int rollNumber = 522141161;
+        int cityPopulation = 10000000;
+        int pincode = 522647;
+        System.out.println(salary);
+
+
+        // variables for long
+        // views, bankBalance, followers
+        long views = 10000000000L;
+        long bankBalance = 2000000000000L;
+        long followers = 50000000000000L;
+        System.out.println("Views: " + views +", Bank ");
+
+    }
+}
